a837694e9071d60177676bf047a1e811951789c4
Misa91 <101438118+Misa91@users.noreply.github.com>
2023-02-24 02:24:47 +0000

    [Finish]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users # deviseを使用する際、URLとしてusersを含む
   root to: "homes#top"
   get 'homes/about', as: 'about'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
